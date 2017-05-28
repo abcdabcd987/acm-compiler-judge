57bccb9949ca20b7c375a90b1446c9063e107b41
Lequn Chen <chenlequn22@gmail.com>
2017-05-28 15:42:09 +0800

    update autostart_tmux_judge.sh

--- autostart_tmux_judge.sh
+++ autostart_tmux_judge.sh
@@ -6,6 +6,14 @@ cd "$(dirname "$0")"
 git pull
 sed -i '/JUDGE_NAME =/d' settings.py
 echo "JUDGE_NAME = 'judge.$ip'" >> settings.py
+
 tmux new-session -d -s judge
-tmux send-keys ./judge/judge.py C-m
+tmux send-keys -t judge:0 "while true; do ./judge/judge.py; sleep 1; done" C-m
+
+tmux new-window -d -t judge
+tmux send-keys -t judge:1 "ping www.baidu.com" C-m
+
+tmux new-window -d -t judge
+tmux send-keys -t judge:2 "htop" C-m
+
 tmux detach -s judge
